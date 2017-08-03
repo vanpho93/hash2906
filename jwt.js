fe1392198baaf9a2cba36570756c5cd08b40d333
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-03 21:27:24 +0700

    xong

---
+++ jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = 'dah239efj2hda89e';
+
+function getToken(obj) {
+    return new Promise((resolve, reject) => {
+        jwt.sign(obj, SECRET_KEY, { expiresIn: 20 }, (err, token) => {
+            if (err) return reject(err);
+            resolve(token);
+        });
+    });
+}
+
+function getObject(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, SECRET_KEY, (err, obj) => {
+            if (err) return reject(err);
+            resolve(obj);
+        });
+    });
+}
+
+// getToken({ name: 'Pho' })
+// .then(token => console.log(token));
+
+getObject('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUGhvIiwiaWF0IjoxNTAxNzcwMzU3LCJleHAiOjE1MDE3NzAzNzd9.QF38vWkKCTO8VkgGpikN3c-s58ss3LzKI0EAXpomH-E')
+.then(obj => console.log(obj));
