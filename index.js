aa2b2461285e96c692dba6a42bc10f791ed770b3
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-05 19:52:45 +0700

    xong

--- index.js
+++ index.js
@@ -8,24 +8,16 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.use(session({
-    secret: 'ge2cqnfjgd7234y',
-    saveUninitialized: true,
-    resave: false,
-    cookie: { maxAge: 30000 }
-}));
-
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/private', (req, res) => {
-    if(!req.session.daDangNhap) return res.redirect('/signin');
-    req.session.count = req.session.count ? req.session.count + 1 : 1;
-    res.send(req.session.user);
+    // TODO HERE
+    // Check log status
 });
 
 function redirectIfLoggedIn(req, res, next) {
-    if(req.session.daDangNhap) return res.redirect('/private');
-    next();
+    // TODO HERE
+    // Check if loggedIn
 }
 
 app.get('/signin', redirectIfLoggedIn, (req, res) => res.render('signin'));
@@ -36,9 +28,9 @@ app.post('/signin', redirectIfLoggedIn, parser, (req, res) => {
     const user = new User(email, password);
     user.signIn()
     .then(user => {
-        req.session.daDangNhap = true;
-        req.session.user = user;
-        res.send('Dang nhap thanh cong');
+        // TODO HERE
+        // tao ra token, req.cookie
+        // res.send() or res.redirect
     })
     .catch(() => res.send('Dang nhap that bai'));
 });
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('home'));
+app.get('/', (req, res) => {
+    res.cookie('token', 'sdiqe9yr9e8w');
+    res.send('home');
+});
+
+app.get('/check', (req, res) => {
+    console.log(req.cookies);
+    res.send('home');
+});
 
 app.listen(3000, () => console.log('Server started!'));
